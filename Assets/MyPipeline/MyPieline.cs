189d4a4c412eabfcb475bffc1d61f73a09e66232
JaibeerRythmos <51321608+JaibeerRythmos@users.noreply.github.com>
2019-07-23 13:43:05 +0530

    Updating The pipeline
    -> now ! I will now try to redo get things
    ->

--- Assets/MyPipeline/MyPieline.cs
+++ Assets/MyPipeline/MyPieline.cs
@@ -6,8 +6,23 @@ using UnityEngine.Rendering;
 
 public class MyPieline : RenderPipeline
 {
+
+    CullingResults cull;
+
+    // Command Buffer
+    // The Idea of command buffer is that it will manage graphis memory but at the same time control what can be done in that memory
+    // For example the CommandBuffer will allow you to render things faster but at the same time it can control wheather you drawMesh or
+    // This will also be added to the camera
+    // Read MOAR at https://docs.unity3d.com/ScriptReference/Rendering.CommandBuffer.html
+    // TODO : Make a seprate function for this so you can may be have more control over this. 
+    CommandBuffer cameraBuffer = new CommandBuffer
+    {
+        name = "Render Camera", //Gives the command the cameras name in the frame debugger to work with can be customised how you want it.
+    };
+
+
     /// <summary>
-    ///Render Pipeline Loop basically everthing that can be rendered renders here and we define it here.
+    /// Render Pipeline Loop basically everthing that can be rendered renders here and we define it here.
     /// </summary>
     /// <param name="context">Context is like HTML5 Canvas context basically in 3D rather than 2D</param> (if you want a condeing example)
     /// <param name="cameras">All the UnityEngine.Camera components.</param>
@@ -15,8 +30,16 @@ public class MyPieline : RenderPipeline
     {
         foreach (var Cam in cameras)
         {
+            //New Builtin Passes for camera? maybe
+            BeginCameraRendering(context, Cam); 
             Render(context, Cam);
+            EndCameraRendering(context, Cam);
         }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        
     }
 
     /// <summary>
@@ -25,21 +48,42 @@ public class MyPieline : RenderPipeline
     /// </summary>
     /// <param name="context">the main screen space context</param>
     /// <param name="camera">the UnityEngine.Camera components</param>
-    void Render(ScriptableRenderContext context,Camera camera)
+    void Render(ScriptableRenderContext context, Camera camera)
     {
-        // Command Buffer
-        // The Idea of command buffer is that it will manage graphis memory but at the same time control what can be done in that memory
-        // For example the CommandBuffer will allow you to render things faster but at the same time it can control wheather you drawMesh or
-        // This will also be added to the camera
-        // Read MOAR at https://docs.unity3d.com/ScriptReference/Rendering.CommandBuffer.html
-        // And yes i 
-        var buffer = new CommandBuffer();
-        context.ExecuteCommandBuffer(buffer); 
+        
+
+        ScriptableCullingParameters cullingParameters;
+        // In the Catlike Codeing they use an outdated metho and class but not you can get the paramters via the camera
+        // using the cullingParamters
+        // This Function allows you keep track of the cameras settings and take care of the culling rules in the provided object of type cullingParameters
+        if (!camera.TryGetCullingParameters(out cullingParameters)) { return; }
+
+#if UNITY_EDITOR
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+#endif
+        // CullingResults was previously called CullResult and not that is depricated i have this here for now if i use it in future.
+        cull = context.Cull(ref cullingParameters);
 
         // Applies MatrixViewProjection from unity to the camera read more on https://learnopengl.com/Getting-started/Coordinate-Systems
         context.SetupCameraProperties(camera);
-        context.DrawSkybox(camera);
+
+        // Acts like GL clear Color or clearning the canvas.
+        // Only difference it uses the Flags to see what features are avalable.
+        // CameraClearFlags :  is an enumeration that can be used as a set of bit flags. Each bit of the value is used to indicate whether a certain feature is enabled or not.
+        // TODO : Figure out what all can be done with flags
+        
+        CameraClearFlags clearFlags = camera.clearFlags;
+        cameraBuffer.ClearRenderTarget(
+            (clearFlags & CameraClearFlags.Depth) != 0,
+            (clearFlags & CameraClearFlags.Color) != 0,
+            camera.backgroundColor
+        );
+        context.ExecuteCommandBuffer(cameraBuffer);
+        cameraBuffer.Clear();
         // And whenver you allow something on the paper you need to make sure to submit it to the teacher which is unity here :P 
-        context.Submit(); 
+        context.Submit();
     }
 }
--- Assets/MyPipeline/MyPipelineAsset.cs
+++ Assets/MyPipeline/MyPipelineAsset.cs
@@ -11,8 +11,9 @@ using UnityEngine.Rendering;
 /// </summary>
 //Adding the new instance of a pipeline in the menu. 
 [CreateAssetMenu(menuName = "Rendering/My Pipeline")]
-public class MyPipelineAsset : RenderPipelineAsset
+public class MyPipelineAsset : RenderPipelineAsset 
 {
+
     protected override RenderPipeline CreatePipeline()
     {
         return new MyPieline(); 
